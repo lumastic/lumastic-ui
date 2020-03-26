8becdc472d3b433101ebce0694d167bc353a9a7f
aml3ed <aml3ed@virginia.edu>
2020-03-26 09:17:01 -0400

    Signature component

--- src/templates/Signature/Signature.js
+++ src/templates/Signature/Signature.js
@@ -2,16 +2,29 @@ import React from "react";
 import PropTypes from "prop-types";
 import style from "./Signature.scss";
 import classNames from "../../helpers/classNames";
+import { Avatar } from "../../components/Avatar";
+import { Type } from "../../components/Type";
 
-const Signature = ({ children, className, ...rest }) => (
-  <div className={classNames(className, style.signature)} data-testid={"signature"} {...rest}>
-    {children}
+const Signature = ({
+  children,
+  className,
+  src = "https://cdn.lumastic.com/defaultAvatar.png",
+  alt = "User Avatar"
+}) => (
+  <div
+    className={classNames(className, style.signature)}
+    data-testid="signature"
+  >
+    <Avatar className={style["sig-avatar"]} src={src} alt={alt} />
+    <Type className={style["sig-text"]}>{children}</Type>
   </div>
 );
 
 Signature.propTypes = {
   children: PropTypes.node,
-  className: PropTypes.string
+  className: PropTypes.string,
+  src: PropTypes.string,
+  alt: PropTypes.string
 };
 
 export { Signature };
