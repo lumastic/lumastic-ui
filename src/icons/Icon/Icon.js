df6a0894a1400ac1a90f754d2fc9d748b0a399e6
aml3ed <aml3ed@virginia.edu>
2020-03-26 14:55:56 -0400

    Got rid of className in Icon

--- src/icons/Icon/Icon.js
+++ src/icons/Icon/Icon.js
@@ -1,17 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
 import style from "./Icon.scss";
-import classNames from "../../helpers/classNames";
 
-const Icon = ({ icon, className }) => (
-  <svg viewBox={icon.viewBox} className={classNames(className, style.icon)}>
+const Icon = ({ icon }) => (
+  <svg viewBox={icon.viewBox} className={style.icon}>
     <use xlinkHref={`#${icon.id}`} />
   </svg>
 );
 
 Icon.propTypes = {
-  icon: PropTypes.object,
-  className: PropTypes.string
+  icon: PropTypes.object
 };
 
 export { Icon };
