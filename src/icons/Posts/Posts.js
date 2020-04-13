5626a651c175f430807b839c7f33c7dd35c6de2d
aml3ed <aml3ed@virginia.edu>
2020-04-13 13:07:38 -0400

    Committing two new icons for SparkCard

---
+++ src/icons/Posts/Posts.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { Icon } from "../Icon";
+import icon from "./Posts.svg";
+
+const Posts = () => <Icon icon={icon} />;
+
+export { Posts };
---
+++ src/icons/Users/Users.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { Icon } from "../Icon";
+import icon from "./Users.svg";
+
+const Users = () => <Icon icon={icon} />;
+
+export { Users };
