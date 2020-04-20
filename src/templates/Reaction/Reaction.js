3edfb114b730d111df5dbe6b554a67ff83215175
aml3ed <aml3ed@virginia.edu>
2020-04-20 13:45:36 -0400

    Added onClick and onRemove handlers

--- src/templates/Reaction/Reaction.js
+++ src/templates/Reaction/Reaction.js
@@ -5,20 +5,54 @@ import classNames from "../../helpers/classNames";
 import { Emoji } from "../../components/Emoji";
 import { Chip } from "../../components/Chip";
 
-const Reaction = ({ className, emoji, reactors }) => (
-  <div className={classNames(className, style.reaction)} data-testid="reaction">
-    <Chip
-      symbol={<Emoji emoji={emoji} className={style.emoji} />}
-      label={`${reactors.length}`}
-      color="grey"
-    />
-  </div>
-);
+const Reaction = ({
+  className,
+  emoji,
+  reactors,
+  onRemove,
+  onClick,
+  canRemove
+}) => {
+  const handleClick = e => {
+    if (onClick) onClick(e);
+  };
+  const handleEnter = e => {
+    if (e.keyCode === 13) {
+      handleClick();
+    }
+  };
+  const handleRemove = e => {
+    e.stopPropagation();
+    if (onRemove && canRemove) onRemove(e);
+  };
+
+  return (
+    <div
+      role="button"
+      tabIndex={0}
+      className={classNames(className, style.reaction)}
+      data-testid="reaction"
+      onClick={handleClick}
+      onKeyDown={handleEnter}
+    >
+      <Chip
+        className={style.chip}
+        symbol={<Emoji emoji={emoji} className={style.emoji} />}
+        label={`${reactors.length}`}
+        color="grey"
+        onRemove={canRemove ? handleRemove : null}
+      />
+    </div>
+  );
+};
 
 Reaction.propTypes = {
   className: PropTypes.string,
   emoji: PropTypes.object,
-  reactors: PropTypes.array
+  reactors: PropTypes.array,
+  onRemove: PropTypes.func,
+  canRemove: PropTypes.bool,
+  onClick: PropTypes.func
 };
 
 export { Reaction };
--- src/templates/Reaction/Reaction.md
+++ src/templates/Reaction/Reaction.md
@@ -16,6 +16,29 @@ import { drew, keith, atishay } from "../../helpers/user.db.js";
 />;
 ```
 
+#### `onClick` & `onRemove`
+
+```jsx
+import { drew, keith, atishay } from "../../helpers/user.db.js";
+<Reaction
+  emoji={{
+    annotation: "waving hand",
+    group: 1,
+    hexcode: "1F44B",
+    order: 163,
+    shortcodes: ["wave"],
+    tags: ["hand", "wave", "waving"],
+    unicode: "👋"
+  }}
+  reactors={[drew, keith, atishay]}
+  onClick={() => alert("You clicked the reaction")}
+  onRemove={() => alert("You clicked to remove")}
+  canRemove
+/>;
+```
+
+**NOTE**: `canRemove` must be set to `true` to use `onRemove`
+
 Source:
 
 ```js { "file": "./Reaction.js" }
