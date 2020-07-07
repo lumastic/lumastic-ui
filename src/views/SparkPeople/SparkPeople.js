5f6f4cd59a59e39cf2283823a0aeffdd7c3d9144
Drew <drew@Drews-MacBook-Pro.local>
2020-07-07 17:14:46 -0400

    Committing changes to sparkPeople for adding collaborators

--- src/views/SparkPeople/SparkPeople.js
+++ src/views/SparkPeople/SparkPeople.js
@@ -3,71 +3,82 @@ import React from "react";
 import {
   Avatar,
   AvatarGroup,
+  Button,
   Card,
-  IconButton,
   Label,
   Link,
   List,
   Tooltip,
-  Button
+  Form
 } from "../../components";
 import classNames from "../../helpers/classNames";
-import { Gear } from "../../icons";
 import { profileRoute } from "../../routes";
+import { SearchSelect } from "../../templates";
 import style from "./SparkPeople.scss";
 
-const SparkPeople = ({ spark = {}, className, isAdmin = false }) => (
+const SparkPeople = ({
+  spark = {},
+  className,
+  isAdmin = false,
+
+  renderCollaboratorResult,
+  renderCollaboratorSelection,
+  onAddCollaborators,
+  onSearchCollaborators
+}) => (
   <Card className={classNames(className, style.sparkinfo)}>
     <List>
       <div>
-        <Label
-          right={
-            isAdmin && (
-              <Link button>
-                <IconButton color="grey">
-                  <Gear />
-                </IconButton>
+        <Label>{`${spark.collaborators?.length} Collaborators`}</Label>
+
+        {isAdmin && (
+          <Form
+            className={style.search}
+            onSubmit={onAddCollaborators}
+            defaultValues={{ collaborators: [] }}
+          >
+            <SearchSelect
+              name="collaborators"
+              placeholder="Add collaborators..."
+              onSearch={onSearchCollaborators}
+              renderResult={renderCollaboratorResult}
+              renderSelection={renderCollaboratorSelection}
+            />
+            <Button size="small" variant="contained" type="submit">
+              Add
+            </Button>
+          </Form>
+        )}
+        <AvatarGroup>
+          {spark.collaborators?.map((collaborator, index) => (
+            <Tooltip
+              label={collaborator.name}
+              position="top"
+              key={collaborator.id || index}
+            >
+              <Link to={profileRoute(collaborator.username)} inline>
+                <Avatar src={collaborator.avatarURL} />
               </Link>
-            )
-          }
-        >{`${spark.collaborators?.length} Collaborators`}</Label>
-        <div className={style.actionrow}>
-          <div className={style.left}>
-            <AvatarGroup>
-              {spark.collaborators?.map((collaborator, index) => (
-                <Tooltip
-                  label={collaborator.name}
-                  position="top"
-                  key={collaborator.id || index}
-                >
-                  <Link to={profileRoute(collaborator.username)} inline>
-                    <Avatar src={collaborator.avatarURL} />
-                  </Link>
-                </Tooltip>
-              ))}
-            </AvatarGroup>
-          </div>
-        </div>
+            </Tooltip>
+          ))}
+        </AvatarGroup>
       </div>
       <div>
         <Label>{`${spark.followers?.length || 0} Followers`}</Label>
-        <div className={style.actionrow}>
-          <div className={style.left}>
-            <AvatarGroup>
-              {spark.followers?.map((follower, index) => (
-                <Tooltip
-                  label={follower.name}
-                  position="top"
-                  key={follower.id || index}
-                >
-                  <Link to={profileRoute(follower.username)} inline>
-                    <Avatar src={follower.avatarURL} key={follower.id} />
-                  </Link>
-                </Tooltip>
-              ))}
-            </AvatarGroup>
-          </div>
-        </div>
+
+        <AvatarGroup>
+          {spark.followers?.map((follower, index) => (
+            <Tooltip
+              label={follower.name}
+              position="top"
+              key={follower.id || index}
+            >
+              <Link to={profileRoute(follower.username)} inline>
+                <Avatar src={follower.avatarURL} key={follower.id} />
+              </Link>
+            </Tooltip>
+          ))}
+        </AvatarGroup>
       </div>
     </List>
   </Card>
@@ -76,7 +87,17 @@ const SparkPeople = ({ spark = {}, className, isAdmin = false }) => (
 SparkPeople.propTypes = {
   spark: PropTypes.object,
   className: PropTypes.string,
-  isAdmin: PropTypes.bool
+  isAdmin: PropTypes.bool,
+  renderCollaboratorResult: PropTypes.oneOfType([
+    PropTypes.func,
+    PropTypes.element
+  ]),
+  renderCollaboratorSelection: PropTypes.oneOfType([
+    PropTypes.func,
+    PropTypes.element
+  ]),
+  onSearchCollaborators: PropTypes.func,
+  onAddCollaborators: PropTypes.func
 };
 
 export { SparkPeople };
