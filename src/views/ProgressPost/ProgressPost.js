0afe9172317c8e7fd68bca895ac956f2c3b261e3
Drew <drew@Drews-MacBook-Pro.local>
2020-07-20 13:21:05 -0400

    Committing new Progress Post header

--- src/views/ProgressPost/ProgressPost.js
+++ src/views/ProgressPost/ProgressPost.js
@@ -1,7 +1,16 @@
 import PropTypes from "prop-types";
 import React from "react";
 import { PressRenderer } from "pressdk";
-import { Card, Divider, Type, Point, MenuItem, Link } from "../../components";
+import {
+  Card,
+  Divider,
+  Type,
+  Point,
+  MenuItem,
+  Link,
+  Avatar,
+  Breadcrumbs
+} from "../../components";
 import {
   Comment,
   Reaction,
@@ -16,7 +25,7 @@ import { pressComponents } from "../../PressHelpers";
 import { CommentForm } from "../../forms";
 import style from "./ProgressPost.scss";
 import { useUser } from "../../hooks";
-import { editPostRoute } from "../../routes";
+import { editPostRoute, viewSparkRoute, profileRoute } from "../../routes";
 
 const ProgressPost = ({
   spark = {},
@@ -30,85 +39,96 @@ const ProgressPost = ({
   const { id } = useUser();
   const isAuthor = post?.createdBy?.id === id;
   return (
-    <div className={style.postcontainer} data-testid="progresspost">
-      <Point className={style.point} withBorder />
-      <Card className={style.progresspost}>
-        <div className={style.postheader}>
-          <SparkCrumbs
-            spark={spark}
-            user={post.createdBy}
-            className={style.crumbs}
-            small
-          />
-          <Type color="grey" className={style.time} tag="div" setSize="0.7rem">
-            {formatTime({ time: post.createdAt || post.time })}
+    <Card className={style.progresspost}>
+      <div className={style.postheader}>
+        <Link to={profileRoute(post?.createdBy?.username)} inline>
+          <Avatar src={post?.createdBy?.src} size="big" />
+        </Link>
+        <div className={style.info}>
+          <Breadcrumbs>
+            <Link to={profileRoute(post?.createdBy?.username)} inline>
+              <Type tag="div">
+                <b>{post?.createdBy?.name}</b>
+              </Type>
+            </Link>
+            <Link to={viewSparkRoute(spark?.belongsTo?.name, spark.id)} inline>
+              <Type tag="div">{spark?.title}</Type>
+            </Link>
+          </Breadcrumbs>
+
+          <Type
+            color="grey"
+            className={style.time}
+            tag="div"
+            caption
+            setSize="0.7rem"
+          >
+            {`${post?.spark?.visibility || "Public"} • ${formatTime({
+              time: post.createdAt || post.time
+            })}`}
           </Type>
-          {isAuthor && (
-            <div className={style.menu}>
-              <MoreMenu position="right">
-                <Link
-                  button
-                  to={editPostRoute(
-                    spark?.belongsTo?.name,
-                    spark?.id,
-                    post?.id
-                  )}
-                >
-                  <MenuItem>
-                    <Type body2>Edit</Type>
-                  </MenuItem>
-                </Link>
-                {/* <MenuItem onClick={deleteHandler}>
+        </div>
+        {isAuthor && (
+          <div className={style.menu}>
+            <MoreMenu position="right">
+              <Link
+                button
+                to={editPostRoute(spark?.belongsTo?.name, spark?.id, post?.id)}
+              >
+                <MenuItem>
+                  <Type body2>Edit</Type>
+                </MenuItem>
+              </Link>
+              {/* <MenuItem onClick={deleteHandler}>
                   <Type body2 color="red">
                     Delete
                   </Type>
                 </MenuItem> */}
-              </MoreMenu>
-            </div>
-          )}
-        </div>
-        <Type tag="div">
-          <PressRenderer
-            components={pressComponents}
-            value={parseContent(post?.content)}
-          />
-        </Type>
-        <div className={style.postreactions}>
-          {post.reactions?.map((reaction, key) => (
-            <Reaction
-              reaction={reaction}
-              onClick={reactionClick}
-              canReact={canComment}
-              key={reaction.id || key}
-            />
-          ))}
-          {canComment ? (
-            <AddEmoji
-              recommended={recommendReactions}
-              onSelect={reactionSelect}
-            />
-          ) : null}
-        </div>
-        {(post.comments || canComment) && <Divider />}
-        {(post.comments || canComment) && (
-          <div className={style.comments}>
-            {post.comments?.map((comment, key) => (
-              <Comment
-                comment={comment}
-                createdBy={comment.createdBy}
-                key={comment.id || key}
-              />
-            ))}
-            {canComment && (
-              <CommentForm
-                defaultValues={{ progressUpdateId: post?.id }}
-                onSubmit={newCommentHandler}
-              />
-            )}
+            </MoreMenu>
           </div>
         )}
-      </Card>
-    </div>
+      </div>
+      <Type tag="div">
+        <PressRenderer
+          components={pressComponents}
+          value={parseContent(post?.content)}
+        />
+      </Type>
+      <div className={style.postreactions}>
+        {post.reactions?.map((reaction, key) => (
+          <Reaction
+            reaction={reaction}
+            onClick={reactionClick}
+            canReact={canComment}
+            key={reaction.id || key}
+          />
+        ))}
+        {canComment ? (
+          <AddEmoji
+            recommended={recommendReactions}
+            onSelect={reactionSelect}
+          />
+        ) : null}
+      </div>
+      {}
+      {(post.comments || canComment) && (
+        <div className={style.comments}>
+          {post.comments?.map((comment, key) => (
+            <Comment
+              comment={comment}
+              createdBy={comment.createdBy}
+              key={comment.id || key}
+            />
+          ))}
+          {canComment && (
+            <CommentForm
+              defaultValues={{ progressUpdateId: post?.id }}
+              onSubmit={newCommentHandler}
+            />
+          )}
+        </div>
+      )}
+    </Card>
   );
 };
 
