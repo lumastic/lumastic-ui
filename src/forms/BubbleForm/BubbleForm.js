3da7fb54b6aa4abff11a772ce65162580863771c
Drew <drew@Drews-MacBook-Pro.local>
2020-11-04 12:55:16 -0500

    Bubble form made

--- src/forms/BubbleForm/BubbleForm.js
+++ src/forms/BubbleForm/BubbleForm.js
@@ -1,16 +1,84 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { useReset } from "../../hooks";
+import {
+  Avatar,
+  Button,
+  Chip,
+  Form,
+  Label,
+  TextInput,
+  Type
+} from "../../components";
+import { SearchSelect, Signature } from "../../templates";
 import style from "./BubbleForm.scss";
 import classNames from "../../helpers/classNames";
 
-const BubbleForm = ({ children, className, ...rest }) => (
-  <div className={classNames(className, style.bubbleform)} data-testid={"bubbleform"} {...rest}>
-    {children}
-  </div>
-);
+const BubbleForm = ({
+  onSubmit,
+  defaultValues = {},
+  buttonLabel = "New Bubble",
+  buttonProps = {},
+  onSearchOrgs,
+  className
+}) => {
+  const [reset, toggle] = useReset();
+  return (
+    <Form
+      className={classNames(style.search, className)}
+      onSubmit={async (data, e, rest) => {
+        const { reset: formReset } = rest;
+        rest.reset = () => {
+          formReset();
+          toggle();
+        };
+        if (onSubmit) {
+          await onSubmit(data, e, rest);
+        } else {
+          alert(JSON.stringify(data));
+        }
+      }}
+      defaultValues={{ collaborators: [], ...defaultValues }}
+    >
+      <div>
+        <Label>Name</Label>
+        <TextInput name="name" placeholder="Bubble name..." />
+      </div>
+      <div>
+        <Label>Share With</Label>
+        <SearchSelect
+          name="collaborators"
+          placeholder="Add people or organizations..."
+          onSearch={onSearchOrgs}
+          renderResult={({ name, avatarURL }) => (
+            <Signature>
+              <Avatar src={avatarURL} size="small" />
+              <Type body2>{name}</Type>
+            </Signature>
+          )}
+          renderSelection={({ name, avatarURL, onRemove }) => (
+            <Chip
+              symbol={<Avatar src={avatarURL} size="small" />}
+              label={name}
+              onRemove={onRemove}
+            />
+          )}
+          reset={reset}
+        />
+      </div>
+      <Button size="small" variant="contained" type="submit" {...buttonProps}>
+        {buttonLabel}
+      </Button>
+    </Form>
+  );
+};
 
 BubbleForm.propTypes = {
-  children: PropTypes.node,
+  onSubmit: PropTypes.func,
+  defaultValues: PropTypes.object,
+  buttonProps: PropTypes.object,
+  buttonLabel: PropTypes.string,
+  onSearchOrgs: PropTypes.func,
   className: PropTypes.string
 };
 
