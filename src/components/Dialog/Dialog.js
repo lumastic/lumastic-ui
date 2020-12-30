845261732450270d8f0d5c6ebab40ba4f150497e
Drew Lytle <aml3ed@virginia.edu>
2020-12-30 16:37:51 -0500

    Don't use useOffclick for Dialog and have custom offClick handler

--- src/components/Dialog/Dialog.js
+++ src/components/Dialog/Dialog.js
@@ -1,7 +1,6 @@
 import PropTypes from "prop-types";
 import React, { createContext, useRef } from "react";
 import classNames from "../../helpers/classNames";
-import { useOffclick } from "../../hooks";
 import { Times } from "../../icons/Times";
 import { IconButton } from "../IconButton";
 import { Modal } from "../Modal";
@@ -17,14 +16,19 @@ const Dialog = ({
   disablePortal = false
 }) => {
   const ref = useRef();
-  useOffclick(ref, hide);
+  // useOffclick(ref, hide);
+  function offClick(e) {
+    if (e.target === e.currentTarget) {
+      hide();
+    }
+  }
   return (
     <Modal isShowing={isShowing} disablePortal={disablePortal}>
       {/* DialogContext allows componenets to get it's properties i.e. a submit button should hide the modal */}
       <DialogContext.Provider value={{ isShowing, hide }}>
         <div className={style["dialog-modal"]}>
           <div className={style["dialog-cover"]} />
-          <div className={style["dialog-area"]}>
+          <div className={style["dialog-area"]} onClick={offClick}>
             <div
               className={classNames(className, style.dialog)}
               data-testid="dialog"
