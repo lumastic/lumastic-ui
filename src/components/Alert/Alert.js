26b9d4d82fcffb1ede824539b077f121a5827581
Drew Lytle <aml3ed@virginia.edu>
2021-02-20 19:14:19 -0500

    Saving this boy

--- src/components/Alert/Alert.js
+++ src/components/Alert/Alert.js
@@ -3,6 +3,14 @@ import React from "react";
 import classNames from "../../helpers/classNames";
 import style from "./Alert.scss";
 
+/**
+ * A component that shows formatted messages
+ * @param {Object} props
+ * @param {*} props.children Children of the component
+ * @param {string} props.className Custom class you can add to the component
+ * @param {("error", "warning", "success", "info)} props.severity The type of alert - defaults to "info"
+ * * @param {("filled", "outlined", "normal")} props.variant The styling of the alert - defaults to "normal"
+ */
 const Alert = ({
   children,
   className,
