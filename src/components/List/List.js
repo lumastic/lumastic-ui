cd9f0071b1360c28d75a59726ee56eb0a61bb8ca
Sarah Overton <sarah.k.overton@gmail.com>
2021-03-31 16:25:34 -0400

    updated List to accommodate horizontal

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,20 +2,43 @@ import React from "react";
 import PropTypes from "prop-types";
 import style from "./List.scss";
 import classNames from "../../helpers/classNames";
+import { Label } from "../Label";
 
-const List = ({ children, className, ...rest }) => (
+const List = ({
+  children,
+  className,
+  label,
+  action,
+  horizontal = false,
+  ...rest
+}) => (
   <div
-    className={classNames(className, style.list)}
+    className={classNames(className, style.listContainer)}
     data-testid="list"
     {...rest}
   >
-    {children}
+    {(label || action) && (
+      <div className={classNames(className, style.topbar)}>
+        <Label className={classNames(className, style.label)}>{label}</Label>
+        <div className={classNames(className, style.action)}>{action}</div>
+      </div>
+    )}
+    <div
+      className={classNames(className, style.list, {
+        [style.horizontal]: horizontal
+      })}
+    >
+      {children}
+    </div>
   </div>
 );
 
 List.propTypes = {
   children: PropTypes.node,
-  className: PropTypes.string
+  className: PropTypes.string,
+  horizontal: PropTypes.bool,
+  label: PropTypes.string,
+  action: PropTypes.node
 };
 
 export { List };
